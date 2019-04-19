47d7986da01a643e5e3400316a7ff33a7ad69c9d
YgYang <ygyang1987@126.com>
2019-04-19 15:31:18 +0800

    Create ascii2num.c
    添加字符串转数值以支持带参数命令

---
+++ ascii2num.c
@@ -0,0 +1,26 @@
+#include <rtthread.h>
+
+rt_uint32_t str2dec(char* str)
+{
+  rt_uint32_t i;
+	rt_uint32_t result = 0;
+  char tmpchar;
+
+	for(i=0; ;++i)
+	{
+		if (0 == (tmpchar = str[i])) 
+		{
+			break;
+		}
+    result *= 10;
+	  if(tmpchar>='0' && tmpchar<='9')
+		{
+      result += tmpchar - '0';
+		}
+		else /* any illegal char return 0 */
+		{
+		  return 0; 
+		}
+	}
+	return result;
+}
