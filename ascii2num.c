e2250f64d57f6aeb2f9913295b39de188fb6d710
YgYang <ygyang1987@126.com>
2019-04-25 16:28:40 +0800

    Update ascii2num.c
    调整了格式

--- ascii2num.c
+++ ascii2num.c
@@ -2,25 +2,25 @@
 
 rt_uint32_t str2dec(char* str)
 {
-  rt_uint32_t i;
-	rt_uint32_t result = 0;
-  char tmpchar;
+    rt_uint32_t i;
+    rt_uint32_t result = 0;
+    char tmpchar;
 
-	for(i=0; ;++i)
-	{
-		if (0 == (tmpchar = str[i])) 
-		{
-			break;
-		}
-    result *= 10;
-	  if(tmpchar>='0' && tmpchar<='9')
-		{
-      result += tmpchar - '0';
-		}
-		else /* any illegal char return 0 */
-		{
-		  return 0; 
-		}
-	}
-	return result;
+    for(i=0; ;++i)
+    {
+        if (0 == (tmpchar = str[i])) 
+        {
+            break;
+        }
+        result *= 10;
+        if(tmpchar>='0' && tmpchar<='9')
+        {
+            result += tmpchar - '0';
+        }
+        else /* any illegal char return 0 */
+        {
+            return 0; 
+        }
+    }
+    return result;
 }
